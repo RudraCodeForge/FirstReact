390011d9299401a93e35e205176f384579bd2dfd
Prince Daksh <149432297+RudraCodeForge@users.noreply.github.com>
2024-07-08 07:37:45 +0000

    Add a show and hide function in sform

--- src/subscription/NewSubscription/NewSubscription.jsx
+++ src/subscription/NewSubscription/NewSubscription.jsx
@@ -1,15 +1,26 @@
 import './NewSubscription.css';
+import {useState} from 'react';
 
 import SForm from './SForm.jsx';
 
 const NewSubscription=(props)=>{
+  const [setForm, setNewForm]=useState(false);
   const onSaveHandler =(data)=>{
     const SubscriptionData={...data,id:Math.random().toString()};
     props.onAddSubbscription(SubscriptionData)
   };
+  const showForm=()=>{
+    if(setForm==false){
+      setNewForm(true)
+    }
+    else{
+      setNewForm(false)
+    }    
+  }
   return(
     <div>
-      <SForm onSave={onSaveHandler}/>
+      {setForm && <SForm onSave={onSaveHandler}/>}
+      <button className="AddNew"type="button" onClick={showForm}>Add NewSubscription</button>
     </div>
   );
 }
