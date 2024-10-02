0959103d89058550db7db36e587bdb397fe2b554
Prince Daksh <149432297+RudraCodeForge@users.noreply.github.com>
2024-10-02 19:54:40 +0000

    Add styled components

--- src/Templates/Container.jsx
+++ src/Templates/Container.jsx
@@ -1,5 +1,7 @@
 import './Container.css';
-const Container=(props)=>{
+import styled from 'styled-components';
+
+/*const Container=(props)=>{
   const classes="container " + props.className
   return(
     <div className={classes} >
@@ -7,4 +9,14 @@ const Container=(props)=>{
     </div>
   );
 }
+export default ; */
+const Container = styled.div `.container{
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-contant:center;
+  width:100%;
+  height:100%;
+  background-color:rgb(5,6,45);
+}`;
 export default Container;
